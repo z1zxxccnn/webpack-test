06faae4763409d9254f41235c6f31b0fec8875cc
z1zxxccnn <z.zxxccnn@163.com>
2023-06-26 01:12:21 +0800

    add eslint cmdline

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,7 +1,7 @@
 declare module '*.svg' {
-  const content: any;
-  export default content;
+  const content: any
+  export default content
 }
 
-declare module "*.module.css";
-declare module "*.module.scss";
+declare module '*.module.css';
+declare module '*.module.scss';
