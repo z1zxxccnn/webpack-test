33f4870c681d8c5e18e54463f5871f13ea9afe96
z1zxxccnn <z.zxxccnn@163.com>
2023-07-24 01:36:55 +0800

    wkwebview clean cache

--- native/webapp_test/webapp_test/mywebview.swift
+++ native/webapp_test/webapp_test/mywebview.swift
@@ -12,13 +12,30 @@ import SwiftUI
 class MyWebViewController: UIViewController {
     var myWebView_: WKWebView? = nil
     
+    deinit {
+        self.cleanWebViewCache()
+    }
+    
     override func loadView() {
+        self.cleanWebViewCache()
         let preferences = WKPreferences()
         let configuration = WKWebViewConfiguration()
         configuration.preferences = preferences
         self.myWebView_ = WKWebView(frame: CGRect.zero, configuration: configuration)
         self.myWebView_?.isInspectable = true
         self.view = self.myWebView_
+    }
+    
+    func cleanWebViewCache() {
+        HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
+        print("all cookies deleted")
+        
+        WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach { record in
+                WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
+                print("record deleted: \(record)")
+            }
+        }
     }
 }
 
