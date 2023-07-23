df0d0a516f5266ca9c437536018c3568b0f277cc
z1zxxccnn <z.zxxccnn@163.com>
2023-07-23 17:27:15 +0800

    init iOS webapp_test

---
+++ native/webapp_test/webapp_test/webapp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webapp_testApp.swift
+//  webapp_test
+//
+//  Created by ztcd on 2023/7/23.
+//
+
+import SwiftUI
+
+@main
+struct webapp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
