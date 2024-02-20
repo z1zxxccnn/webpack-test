b854679c8a35e390e038980f971d0b4aad2a41be
z1zxxccnn <z.zxxccnn@163.com>
2024-02-21 01:12:50 +0800

    uninstall before run; add INTERNET permission; create WebView compose

--- native/Android/webapp_test/app/src/main/java/com/example/webapp_test/MainActivity.kt
+++ native/Android/webapp_test/app/src/main/java/com/example/webapp_test/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.webapp_test
 
+import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webapp_test.ui.theme.Webapp_testTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Webapp_testTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyContent()
             }
         }
     }
 }
 
+// Creating a composable
+// function to create WebView
+// Calling this function as
+// content in the above function
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyContent(){
+
+    // Declare a string that contains a url
+    val mUrl = "https://www.google.com/"
+
+    // Adding a WebView inside AndroidView
+    // with layout as full screen
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+    }, update = {
+        it.loadUrl(mUrl)
+    })
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Webapp_testTheme {
-        Greeting("Android")
+        MyContent()
     }
 }
