482e8655eef7e8543879225b9c4443b4472306e7
z1zxxccnn <z.zxxccnn@163.com>
2024-02-21 21:55:28 +0800

    communication between kotlin and js in Android WebView

--- native/Android/webapp_test/app/src/main/java/com/example/webapp_test/MainActivity.kt
+++ native/Android/webapp_test/app/src/main/java/com/example/webapp_test/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.webapp_test
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webapp_test.ui.theme.Webapp_testTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,12 +28,42 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun injectJavaScript(webView: WebView, script: String) {
+    webView.evaluateJavascript(script, null)
+}
+
+class CustomWebViewClient : WebViewClient() {
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        //super.onPageStarted(view, url, favicon)
+
+        // Inject JavaScript
+        val script = "console.log('Hello, Android JS Injection!');"
+        if (view != null) {
+            injectJavaScript(view, script)
+        }
+    }
+}
+
+class AndroidJSInterface(private val context: Context) {
+
+    /** Show a toast from the web page  */
+    @JavascriptInterface
+    fun showToast(toast: String?) {
+        Toast.makeText(context, toast, Toast.LENGTH_SHORT).show()
+    }
+
+    @JavascriptInterface
+    fun testCall(msg: String): String {
+        return "return from kotlin: $msg"
+    }
+}
+
 // Creating a composable
 // function to create WebView
 // Calling this function as
 // content in the above function
 @Composable
-fun MyContent(){
+fun MyContent() {
 
     // Declare a string that contains a url
     val mUrl = "http://127.0.0.1:9090/"
@@ -46,11 +77,15 @@ fun MyContent(){
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-            {
+            webViewClient = CustomWebViewClient()
+
+            settings.javaScriptEnabled = true
+
+            addJavascriptInterface(AndroidJSInterface(it), "Android")
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 WebView.setWebContentsDebuggingEnabled(true)
             }
-            settings.javaScriptEnabled = true
         }
     }, update = {
         it.loadUrl(mUrl)
