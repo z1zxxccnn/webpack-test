31cf8304436f090be33d8e6a8605a73fb08e146b
z1zxxccnn <z.zxxccnn@163.com>
2024-03-18 01:07:00 +0800

    init macOS webapp_test

---
+++ native/macOS/webapp_test/webapp_test/webapp_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webapp_testApp.swift
+//  webapp_test
+//
+//  Created by ztcd on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct webapp_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
